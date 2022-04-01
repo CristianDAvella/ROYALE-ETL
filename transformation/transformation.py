314b8009503ab75640b6fff361ef52f48cc70e08
Cristian <cristianprietoavella@gmail.com>
2022-04-01 12:47:34 -0500

    Problema con base de datos

--- transformation/transformation.py
+++ transformation/transformation.py
@@ -10,3 +10,7 @@ data_transformed = data['cost'].astype('int64', copy=True, errors='raise')
 
 
 print(data_transformed.info())
+
+# Bifurcacion
+# Problema: el motor mysql no responde
+# intentar: https://medium.com/el-acordeon-del-programador/iniciando-mysql-manualmente-en-windows-47a987aa6cff
