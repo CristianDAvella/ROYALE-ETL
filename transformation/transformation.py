7c2c4c445254fc2ca2e9688ed1d155e9d07f9a48
Cristian <cristianprietoavella@gmail.com>
2022-04-02 15:00:53 -0500

    intentando detectar los valores no numericos de la columna cost

--- transformation/transformation.py
+++ transformation/transformation.py
@@ -1,15 +1,30 @@
+from operator import is_not
 import pandas as pd
 
 # Estado actual: arroja el error -> ValueError: invalid literal for int() with base 10: '? '
 # Siguiente paso: probar transformando la columna names. 
-# Objetivo del dia: 
+# Objetivo del dia: crear una funcion que detecte si los datos de una columna son numericos.
 
-data =  pd.read_csv('../scrapy_royale/cards.csv')
+def is_number(colum:pd.Series):
+    positions = []
+    numbers = ['1','2','3','4','5','6','7','8','9','10','0']
 
-data_transformed = data['cost'].astype('int64', copy=True, errors='raise')
+    for data in  colum:
+        if not(data in numbers):
+            positions = colum.isin([data])
+
+    return positions
 
 
-print(data_transformed.info())
+if __name__ == '__main__':
+
+    data =  pd.read_csv('../scrapy_royale/cards.csv')
+
+#    data_transformed = data.astype({'rarity':'category'})
+
+ #   print(data_transformed.info())
+
+    print(is_number(data['cost']))
 
 # Bifurcacion
 # Problema: el motor mysql no responde
